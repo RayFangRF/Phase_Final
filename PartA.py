2c6cf41782c2dcc0725fe07248670a1c699df7d6
thanoslover <acastellanoslh@gmail.com>
2021-05-01 16:28:20 -0400

    asthetic changes

--- PartA.py
+++ PartA.py
@@ -1,15 +1,11 @@
-import sys
-
 import numpy as np
 from scipy import integrate
 from matplotlib import pyplot as plt
 from shapely.geometry import LineString
-from scipy.interpolate import InterpolatedUnivariateSpline
-from matplotlib.ticker import FormatStrFormatter, FixedLocator
 import CubicEquationSolver
 
 n = 1000000
-R = 8.314  # universal gas constant, m3-bar/K-mol
+R = 8.314  # universal gas constant, m3-Pa/K-mol
 # for oxygen:
 Pc = 50.0
 Tc = 154.58
@@ -37,9 +33,9 @@ def Vm_Calc(T, P):
     b = 0.07780 * ((R * Tc) / Pc)
     A = a * P / (R ** 2 * T ** 2)
     B = (b * P) / (R * T)
-    print(A,B)
+    print(A, B)
     liquid_root = CubicEquationSolver.solve(1, (B - 1), (A - 3 * B ** 2 - 2 * B), (A * B - B ** 2 - B ** 3)) * R * T / P
-    return liquid_root#[np.where(liquid_root > 0, liquid_root, np.inf).argmax() - 1]
+    return liquid_root[np.where(liquid_root > 0, liquid_root, np.inf).argmax() - 1]
 
 
 # Solves for the pressures of an T degree Isotherm given a set of molar volumes
@@ -52,6 +48,7 @@ def data_gen(T, Vm):
     P = ((R * T) / (Vm - b)) - ((a * alpha) / (Vm ** 2 + 2 * b * Vm - b ** 2))
     return P
 
+
 def data_gen2(Vm):
     T = 100
     Tr_NO = T / Tc_NO
@@ -63,51 +60,41 @@ def data_gen2(Vm):
     return P
 
 
-# Obtains values to plot NO graph, including P_min and P_max
+# Obtains values to plot NO graph
 def Nitric_Oxide_Graph(T):
     # Initializing function molar volume (x) and pressure values (y)
-    
+
     Vm_list = np.linspace(2.0e-5, 50, n)
     p_list = np.zeros(n)
 
-    # Initializing min/max values and indices
-    P_min = 0
-    P_max = 0
-    P_minIndex = 0
-    P_maxIndex = 0
 
     # Solves for pressure values using data_gen
     for i in range(n):
         p_list[i] = data_gen(T, Vm_list[i])
-        if (p_list[i - 1] < 0 and p_list[i] > 0):
-            print()#p_list[i - 1], " ", p_list[i]
-        if (p_list[i] < P_min):
-            P_min = p_list[i]
-            P_minIndex = i
-            #print(P_minIndex)
+    return Vm_list, p_list
 
-    # Sorting to find maximum
-    for i in range(P_minIndex, n):
+def Nitric_Oxide_Graph_Visual(T):
+    # Initializing function molar volume (x) and pressure values (y)
 
-        if (p_list[i] > P_max):
-            P_max = p_list[i]
-            P_maxIndex = i
-
-    # Creates coordinate points for min and max
-    a = [Vm_list[P_minIndex], P_min]
-    b = [Vm_list[P_maxIndex], P_max]
-
-    return Vm_list, p_list, a, b
+    Vm_list = np.linspace(2.0e-5, 1, n)
+    p_list = np.zeros(n)
 
 
-def plot_NO_100():
-    x, y, a, b = Nitric_Oxide_Graph(100)
+    # Solves for pressure values using data_gen
+    for i in range(n):
+        p_list[i] = data_gen(T, Vm_list[i])
+    return Vm_list, p_list
+
+
+def plot_NO_100(visual):
+    if visual:
+        x, y = Nitric_Oxide_Graph_Visual(100)
+    else:
+        x, y = Nitric_Oxide_Graph(100)
     fig = plt.figure()
     fig.set_size_inches(10.5, 10.5)
     ax = fig.add_subplot(1, 1, 1)
     ax.plot(x, y, color='blue', lw=2, label="T=100k")
-    #idx = np.argwhere(np.diff(np.sign(g-y))).flatten()
-    #ax.plot(x[idx], y[idx], 'ro')
     ax.set_xlabel('Molar Volume(m^3/mol)')
     ax.set_ylabel('Pressure(Pa)')
     ax.set_xscale('log')
@@ -117,69 +104,55 @@ def plot_NO_100():
 
     plt.show()
 
-    return a, b
 
-
-def big_chungus(pressure_guess):
-    x, y, a, b = Nitric_Oxide_Graph(100)
+def Area_Calc(pressure_guess, x, y):
     f = np.linspace(1.0e-5, 1.0, 1000000)
     g = np.full(len(f), pressure_guess)
     line_1 = LineString(np.column_stack((f, g)))
     line_2 = LineString(np.column_stack((x, y)))
     intersection = line_1.intersection(line_2)
     print(intersection)
+    AreaI = integrate.quad(data_gen2, intersection[0].x, intersection[1].x)
+    AreaII = integrate.quad(data_gen2, intersection[1].x, intersection[2].x)
 
-    third_intersect = 0
-    for i in range(n):
-            if(int(y[i]) == int(pressure_guess)):
-                third_intersect = x[i]
-                print(third_intersect)
-                break
-
-    x_y_curve1 = np.column_stack((f, g))
-    x_y_curve2 = np.column_stack((x, y))
-
-    mom= integrate.quad(data_gen2,intersection[0].x, intersection[1].x)
-    dad= integrate.quad(data_gen2, intersection[1].x, third_intersect)
-    print(abs(mom[0]), " ", abs(dad[0]))
+    return abs(AreaI[0]), abs(AreaII[0])
 
 
-def Guess_n_check(pressure_guesses):
-    i = len(pressure_guesses / 2)
-    AreaI, AreaII = big_chungus(pressure_guesses[i])
-    if abs(1 - AreaI / AreaII) > 0.01:
+def Guess_n_check(pressure_guesses, x, y):
+    i = int(len(pressure_guesses)/2)
+    AreaI, AreaII = Area_Calc(pressure_guesses[i],x, y)
+    if abs(1 - AreaI / AreaII) < 0.01:
         return pressure_guesses[i]
     while True:
         if AreaI > AreaII:
             i -= 1
-            AreaI, AreaII = big_chungus(pressure_guesses[i])
-            if abs(1 - AreaI / AreaII) > 0.01:
+            AreaI, AreaII = Area_Calc(pressure_guesses[i],x,y)
+            if abs(1 - AreaI / AreaII) < 0.01:
                 return pressure_guesses[i]
             elif AreaII > AreaI:
-                new_pressures = np.linspace(pressure_guesses[i], pressure_guesses[i+1], 50)
-                Guess_n_check(new_pressures)
+                new_pressures = np.linspace(pressure_guesses[i], pressure_guesses[i + 1], 100)
+                Guess_n_check(new_pressures,x,y)
         elif AreaII > AreaI:
             i += 1
-            AreaI, AreaII = big_chungus(pressure_guesses[i])
-            if abs(1 - AreaI / AreaII) > 0.01:
+            AreaI, AreaII = Area_Calc(pressure_guesses[i],x,y)
+            if abs(1 - AreaI / AreaII) < 0.01:
                 return pressure_guesses[i]
             elif AreaI > AreaII:
-                new_pressures = np.linspace(pressure_guesses[i-1], pressure_guesses[i], 50)
-                Guess_n_check(new_pressures)
-
-
+                new_pressures = np.linspace(pressure_guesses[i - 1], pressure_guesses[i], 100)
+                Guess_n_check(new_pressures,x,y)
 
 
 while True:
     choice = input("Part A" + '\n' + '1.Calculate Pressure for O2 with Temperature and Molar Volume' + '\n' +
-                   '2. Calculate molar volume for O2 using Temperature and Pressure' + '\n' + '3. Display the Vm vs P plot for Nitric Oxide at T = 100k')
+                   '2. Calculate molar volume for O2 using Temperature and Pressure' + '\n' + '3. Display the Vm vs P plot for Nitric Oxide at T = 100k'
+                   + '\n' + '4. Find the vapor pressure for the Isotherm at 100 K')
     choice = int(choice)
     if choice == 1:
         temp = input('Enter the temperature:')
         temp = float(temp)
         Vm = input('Enter the Molar Volume:')
         Vm = float(Vm)
-        print('The Pressure is:', pressure_Calc(temp, Vm), 'bar')
+        print('The Pressure is:', pressure_Calc(temp, Vm), 'Pa')
     elif choice == 2:
         temp = input('Enter the temperature:')
         temp = float(temp)
@@ -187,16 +160,26 @@ while True:
         P = float(P)
         print('The Molar Volume is:', Vm_Calc(temp, P), 'm^3/mol')
     elif choice == 3:
-        a, b = plot_NO_100()
-        #print(a[0], " ", a[1])
-        #print(b[0], " ", b[1])
+        plot_NO_100(True)
 
-        # Part B) Using the algorithm to find P_vap
-        # Beginning with a guess of 0
-        p_guess = 0.0
-        plt.show()
     elif choice == 4:
-        a = input('what is big chungus')
-        big_chungus(a)
-
-        
+        x,y = Nitric_Oxide_Graph(100)
+        guesses = np.linspace(71000, 729231, 100)
+        Pvap = Guess_n_check(guesses,x,y)
+        print("The vapor pressure for the Nitric Oxide Isotherm at 100 K is: ", " ",Pvap, " Pa")
+        x, y = Nitric_Oxide_Graph_Visual(100)
+        fig = plt.figure()
+        fig.set_size_inches(10.5, 10.5)
+        ax = fig.add_subplot(1, 1, 1)
+        ax.plot(x, y, color='blue', lw=2, label="T=100k")
+        ax.set_xlabel('Molar Volume(m^3/mol)')
+        ax.set_ylabel('Pressure(Pa)')
+        ax.set_xscale('log')
+        ax.grid(linestyle='--')
+        ax.set_xlim(1e-5, 0.004)
+        plt.title('Vm vs P for NO')
+        plt.legend()
+        f = np.linspace(1.0e-5, 50, 1000000)
+        g = np.full(len(f), Pvap)
+        ax.plot(f,g, color='red', lw=2)
+        plt.show()
